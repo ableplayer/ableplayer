690feca9f6c1e1e8c346f610d303e4d4a02232d1
Terrill Thompson <terrill@terrillthompson.com>
2014-06-24 23:28:01 -0700

    Add translations directory and English language file

---
+++ translations/en.js
@@ -0,0 +1,113 @@
+{
+
+"play": "Play", 
+
+"pause": "Pause",
+
+"stop": "Stop",
+
+"rewind": "Rewind",
+
+"forward": "Forward",
+
+"seconds": "seconds",
+
+"toggle": "Toggle",
+
+"turnOn": "Turn on",
+
+"turnOff": "Turn off",
+
+"up": "up",
+
+"down": "down",
+
+"show": "Show",
+
+"hide": "Hide",
+
+"captions": "captions",
+
+"descriptions": "description",
+
+"sign": "sign language",
+
+"transcript": "transcript",
+
+"mute": "Mute",
+
+"unmute": "Unmute",
+
+"volume": "Volume", 
+
+"settings": "Settings",
+
+"fullScreen": "Full screen",
+
+"audio": "audio",
+
+"video": "video",
+
+"statusPlaying": "Playing",
+
+"statusPaused": "Paused",
+
+"statusStopped": "Stopped",
+
+"statusWaiting": "Waiting",
+
+"statusUsingDesc": "Using described version",
+
+"statusLoadingDesc": "Loading described version",
+
+"statusUsingNodesc": "Using non-described version",
+
+"statusLoadingNext": "Loading next track",
+
+"statusEnd": "End of track",
+
+"selectedTrack": "Selected Track",
+
+"errorNoPlay": "Sorry,your browser is unable to play this",
+
+"prefTitle": "Preferences",
+
+"prefIntro": "Saving your preferences requires cookies.",
+
+"prefFeatures": "Features",
+
+"prefKeys": "Modifier Keys",
+
+"prefAltKey": "Alt key",
+
+"prefCtrlKey": "Control key",
+
+"prefCaptions": "Closed captions on by default",
+
+"prefDesc": "Description on by default",
+
+"prefClosedDesc": "Use text-based description if available",
+
+"prefVisibleDesc": "If using text-based description,make it visible",
+
+"prefHighlight": "Highlight transcript as media plays",
+
+"prefTabbable": "Keyboard-enable transcript",
+
+"prefSuccess": "Your changes have been saved.",
+
+"prefNoChange": "You didn't make any changes.",
+
+"helpTitle": "Help",
+
+"helpKeys": "The media player on this web page can be operated from anywhere on the page using the following keystrokes:",
+
+"helpKeysDisclaimer1": "Note that modifier keys (Alt and Control) can be assigned within <em>Preferences</em>. Also, some shortcut key combinations might conflict with keys used by your browser and/or assistive technologies. Try various combinations of modifier keys to find one that works for you.",
+
+"save": "Save",
+
+"cancel": "Cancel",
+
+"ok": "ok"
+
+}
