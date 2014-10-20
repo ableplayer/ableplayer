202249b1f5ee31cee5e51ae1e6c1f6be0e90c177
Terrill Thompson <terrill@terrillthompson.com>
2014-10-20 08:54:30 -0700

    Change default translations dir so test pages work

--- build/ableplayer.js
+++ build/ableplayer.js
@@ -256,7 +256,7 @@
     this.langOverride = true;
     
     // translationDir - specify path to translation files 
-    this.translationDir = '/translations/';
+    this.translationDir = '../translations/';
 
     this.setButtonImages();
   };
