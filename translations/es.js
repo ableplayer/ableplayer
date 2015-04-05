55f6f2261c4310e4def018c27d424b6631d180db
Egyrs <emmanuelle@sidar.org>
2015-04-05 19:43:27 +0200

    Spanish translation
    Front-end in spanish

---
+++ translations/es.js
@@ -0,0 +1,185 @@
+{
+  
+"playerHeading": "Media player",
+
+"faster": "Rápido",
+
+"slower": "Lento",
+
+"play": "Play", 
+
+"pause": "Pausa",
+
+"stop": "Detener",
+
+"rewind": "Rebobinar",
+
+"forward": "Adelantar",
+
+"captions": "Subtítulos",
+
+"showCaptions": "Mostrar subtítulos",
+
+"hideCaptions": "Ocultar subtítulos",
+
+"captionsOff": "Quitar subtítulos",
+
+"showTranscript": "Mostrar transcripción",
+
+"hideTranscript": "Ocultar transcripción", 
+
+"turnOnDescriptions": "Habilitar descripciones", 
+
+"turnOffDescriptions": "Deshabilitar descripciones", 
+
+"chapters": "Capítulos",
+
+"language": "Idioma",
+
+"sign": "Lengua de señas",
+
+"showSign": "Mostrar lengua de señas",
+
+"hideSign": "Ocultar lengua de señas",
+
+"mute": "Silenciar",
+
+"unmute": "Reactivar sonido",
+
+"volume": "Volumen", 
+
+"volumeUp": "Subir volumen",
+
+"volumeDown": "Bajar volumen",
+
+"preferences": "Preferencias",
+
+"enterFullScreen": "Ver a pantalla completa",
+
+"exitFullScreen": "Salir de pantalla completa",
+
+"fullScreen": "Pantalla completa",
+
+"speed": "Velocidad",
+
+"audio": "Audio",
+
+"video": "Video",
+
+"or": "o", 
+
+"spacebar": "Barra espaciadora",
+
+"autoScroll": "Desplazamiento automático",
+
+"unknown": "Desconocido", 
+
+"statusPlaying": "Reproduciendo",
+
+"statusPaused": "Pausado",
+
+"statusStopped": "Detenido",
+
+"statusWaiting": "Esperando",
+
+"statusBuffering": "Almacenando",
+
+"statusUsingDesc": "Utilizando versión descrita",
+
+"statusLoadingDesc": "Cargando versión descrita",
+
+"statusUsingNoDesc": "Utilizando versión no descrita",
+
+"statusLoadingNoDesc": "Cargando versión no descrita",
+
+"statusLoadingNext": "Cargando la siguiente pista",
+
+"statusEnd": "Fin de pista",
+
+"selectedTrack": "Pista seleccionada",
+
+"fallbackError1": "Lo sentimos, su navegador no puede reproducir esto",
+
+"fallbackError2": "Los siguientes navegadores se sabe pueden trabajar con este reproductor",
+
+"orHigher": "o superior",
+
+"prefTitle": "Preferencias",
+
+"prefIntro": "Guardar sus preferencias requiere el uso de cookies.",
+
+"prefFeatures": "Características",
+
+"prefKeys": "Teclas modificadoras",
+
+"prefAltKey": "Alt",
+
+"prefCtrlKey": "Control",
+
+"prefShiftKey": "Mayúscula",
+
+"prefCaptions": "Subtítulos habilitados por defecto",
+
+"prefSignLanguage": "Mostrar lengua de señas si está disponible",
+
+"prefDesc": "Habilitar descripción por defecto",
+
+"prefClosedDesc": "Utilizar descripciones en texto si están disponibles",
+
+"prefDescPause": "Pausar automáticamente el video cuando arranque una descripción en texto",
+
+"prefVisibleDesc": "Hacer visibles las descripciones en texto si se están usando",
+
+"prefTranscript": "Habilitar transcripción por defecto",
+
+"prefHighlight": "Resaltar la transcripción según avanza el contenido",
+
+"prefTabbable": "Transcripción manejable por teclado",
+
+"prefSuccess": "Los cambios han sido guardados.",
+
+"prefNoChange": "No se ha hecho ningún cambio.",
+
+"help": "Ayuda",
+
+"helpTitle": "Ayuda",
+
+"helpKeys": "El reproductor en esta página pude ser manejado desde cualquier parte de la pa´gina utilizando los siguientes atajos de teclado:",
+
+"helpKeysDisclaimer": "Tengan en cuenta que las teclas modificadoras (Mayúsculas, Alt, y Control) pueden ser asignadas en las preferencias. Algunas combinaaciones de atajos de teclado pueden entrar en conflicto con teclas utilizadas por su navegador y/o otras aplicaciones. Pruebe varias combinaciones de teclas modificadoras hasta encontrar la que funcione en su caso.",
+
+"save": "Guardar",
+
+"cancel": "Cancelar",
+
+"ok": "ok", 
+
+"done": "Hecho",
+
+"closeButtonLabel": "Cerrar cuadro de diálogo",
+
+"windowButtonLabel": "Opciones en Windows",
+
+"windowMove": "Mover", 
+
+"windowMoveAlert": "Arrastre o use las teclas de flecha para mover la ventana, pulse Enter para parar.",
+
+"windowResize": "Redimensionar", 
+
+"windowResizeHeading": "Redimensionar la ventana con el intérprete",
+
+"windowResizeAlert": "La ventana ha sido redimensionada.",
+
+"width": "Ancho",
+
+"height": "Alto",
+
+"windowSendBack": "Enviar atrás", 
+
+"windowSendBackAlert": "Esta ventana no se encuentra tras otros objetos en la página.",
+
+"windowBringTop": "Traer al frente",
+
+"windowBringTopAlert": "Esta ventan está ahora en el frente de otros objetos en la página."
+
+}
