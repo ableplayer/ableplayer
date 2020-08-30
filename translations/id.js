cbf15216ec5ce587d4633e33fc1a4fafd1d478e3
Fri Rasyidi <fri@frirasyidi.com>
2020-08-31 00:06:08 +0700

    Adding Bahasa Indonesia (id) translation
    Bahasa Indonesia is Indonesian language

---
+++ translations/id.js
@@ -0,0 +1,337 @@
+var id = {
+
+"audio": "audio",
+
+"video": "video",
+
+"playerHeading": "Pemutar media",
+
+"faster": "Lebih cepat",
+
+"slower": "Lebih lambat",
+
+"play": "Putar",
+
+"pause": "Pause",
+
+"stop": "Hentikan",
+
+"restart": "Reset",
+
+"prevChapter": "Bagian sebelumnya",
+
+"nextChapter": "Bagian berikutnya",
+
+"prevTrack": "Trek sebelumnya",
+
+"nextTrack": "Trek berikutnya",
+
+"rewind": "Mundur",
+
+"forward": "Maju",
+
+"captions": "Teks",
+
+"showCaptions": "Tampilkan teks",
+
+"hideCaptions": "Sembunyikan teks",
+
+"captionsOff": "Matikan teks",
+
+"showTranscript": "Tampilkan transkrip",
+
+"hideTranscript": "Sembunyikan transkrip",
+
+"turnOnDescriptions": "Nyalakan deskripsi",
+
+"turnOffDescriptions": "Matikan deskripsi",
+
+"chapters": "Bagian",
+
+"newChapter": "Bagian baru",
+
+"language": "Bahasa",
+
+"sign": "Bahasa isyarat",
+
+"showSign": "Tampilkan bahasa isyarat",
+
+"hideSign": "Sembunyikan bahasa isyarat",
+
+"seekbarLabel": "Lini masa",
+
+"mute": "Bisukan",
+
+"unmute": "Suarakan",
+
+"volume": "Volume",
+
+"volumeHelp": "Klik untuk mengakses pengatur volume",
+
+"volumeUpDown": "Perbesar volume",
+
+"volumeSliderClosed": "Tutup pengatur volume",
+
+"preferences": "Preferensi",
+
+"enterFullScreen": "Masuk ke mode layar penuh",
+
+"exitFullScreen": "Keluar dari mode layar penuh",
+
+"fullScreen": "Layar penuh",
+
+"speed": "Kecepatan",
+
+"and": "dan",
+
+"or": "atau",
+
+"spacebar": "kunci spasi",
+
+"transcriptTitle": "Transkrip",
+
+"lyricsTitle": "Lirik",
+
+"autoScroll": "Otomatis bergulir",
+
+"unknown": "Tidak diketahui",
+
+"statusPlaying": "Diputar",
+
+"statusPaused": "Dipause",
+
+"statusStopped": "Dihentikan",
+
+"statusWaiting": "Menunggu",
+
+"statusBuffering": "Sedang bufer",
+
+"statusUsingDesc": "Menggunakan versi yang tertera",
+
+"statusLoadingDesc": "Memuat versi yang tertera",
+
+"statusUsingNoDesc": "Menggunakan versi yang bukan tertera",
+
+"statusLoadingNoDesc": "Memuat versi yang bukan tertera",
+
+"statusLoadingNext": "Memuat trek berikutnya",
+
+"statusEnd": "Akhir trek",
+
+"selectedTrack": "Trek pilihan",
+
+"alertDescribedVersion": "Menggunakan versi audio yang tertera dari video ini",
+
+"alertNonDescribedVersion": "Bukan menggunakan versi audio yang tertera dari video ini",
+
+"fallbackError1": "Maaf, browser Anda tidak bisa memainkan ini",
+
+"fallbackError2": "Browser berikut diketahui tidak dapat menggunakan pemutar video ini",
+
+"orHigher": "atau lebih tinggi",
+
+"prefMenuCaptions": "Teks",
+
+"prefMenuDescriptions": "Deskripsi",
+
+"prefMenuKeyboard": "Kibor",
+
+"prefMenuTranscript": "Transkrip",
+
+"prefTitleCaptions": "Preferensi Teks",
+
+"prefTitleDescriptions": "Preferensi Deskripsi Audio",
+
+"prefTitleKeyboard": "Preferensi Kibor",
+
+"prefTitleTranscript": "Preferensi Transkrip",
+
+"prefIntroCaptions": "Preferensi berikut mengontrol cara menampilkan teks.",
+
+"prefIntroDescription1": "Pemutar media ini mendukung deskripsi audo dengan dua cara: ",
+
+"prefIntroDescription2": "Video ini memiliki ",
+
+"prefIntroDescriptionNone": "Video ini tidak memiliki deskripsi audio dalam format apa pun.",
+
+"prefIntroDescription3": "Gunakan formulir berikut untuk memasang preferensi terkait deskripsi audio berbasis teks.",
+
+"prefIntroDescription4": "Setelah menyimpan pengaturan Anda, deskripsi audio dapat dinyala/matikan melalui tombol Deskripsi.",
+
+"prefIntroKeyboard1": "Pemutar media dalam halaman ini dapat dioperasikan dari bagian manapun di halaman dengan menggunakan pintasan kibor (lihat daftar di bawah).",
+
+"prefIntroKeyboard2": "Kunci pengubah (Shift, Alt, Control) dapat diberikan peruntukan di bawah.",
+
+"prefIntroKeyboard3": "CATATAN: Beberapa kombinasi kunci mungkin akan konflik dengan yang digunakan oleh browser Anda dan/atau aplikasi perangkat lunak lainnya. Cobalah beragam kombinasi untuk menemukan yang bekerja untuk Anda.",
+
+"prefIntroTranscript": "Preferensi berikut mempengarushi transkrip interaktif.",
+
+"prefCookieWarning": "Untuk menyimpan preferesi dibutuhkan kukis.",
+
+"prefHeadingKeyboard1": "Kunci pengubah untuk pintasan",
+
+"prefHeadingKeyboard2": "Kunci pintasan saat ini",
+
+"prefHeadingDescription": "Deskripsi audio",
+
+"prefHeadingTextDescription": "Deskripsi audio basis teks",
+
+"prefHeadingCaptions": "Teks",
+
+"prefHeadingTranscript": "Transkrip Interaktid",
+
+"prefAltKey": "Alt",
+
+"prefCtrlKey": "Control",
+
+"prefShiftKey": "Shift",
+
+"escapeKey": "Escape",
+
+"escapeKeyFunction": "Tutup menu dialog atau popup ini",
+
+"prefDescFormat": "Format yang disukai",
+
+"prefDescFormatHelp": "Jika kedua format tersedia, hanya satu yang akan digunakan.",
+
+"prefDescFormatOption1": "versi alternatif video yang tertera",
+
+"prefDescFormatOption1b": "versi alternatif yang tertera",
+
+"prefDescFormatOption2": "deskripsi berbasis teks, dibacakan oleh pembaca layar",
+
+"prefDescFormatOption2b": "deskripsi berbasis teks",
+
+"prefDescPause": "Otomatis pause video ketika deskripsi dimulai",
+
+"prefVisibleDesc": "Menampakkan deskripsi",
+
+"prefHighlight": "Sorotu transkrip saat media diputar",
+
+"prefTabbable": "Transkrip dengan kontrol kibor",
+
+"prefCaptionsFont": "Jenis Huruf",
+
+"prefCaptionsColor": "Warna Teks",
+
+"prefCaptionsBGColor": "Warna Latar",
+
+"prefCaptionsSize": "Ukuran Teks",
+
+"prefCaptionsOpacity": "Opasitas",
+
+"prefCaptionsStyle": "Gaya",
+
+"serif": "serif",
+
+"sans": "sans-serif",
+
+"cursive": "huruf sambung",
+
+"fantasy": "fantasi",
+
+"monospace": "monospace",
+
+"white": "putih",
+
+"yellow": "kuning",
+
+"green": "hijau",
+
+"cyan": "sian",
+
+"blue": "biru",
+
+"magenta": "magenta",
+
+"red": "merah",
+
+"black": "hitam",
+
+"transparent": "transparan",
+
+"solid": "solid",
+
+"captionsStylePopOn": "Muncul",
+
+"captionsStyleRollUp": "Bergulir",
+
+"prefCaptionsPosition": "Posisi",
+
+"captionsPositionOverlay": "Menindih video",
+
+"captionsPositionBelow": "Di bawah video",
+
+"sampleCaptionText": "Contoh tampilan teks",
+
+"prefSuccess": "Pengubahan yang Anda buat sudah disimpan.",
+
+"prefNoChange": "Anda tidak mengubah apa pun.",
+
+"help": "Bantuan",
+
+"helpTitle": "Bantuan",
+
+"save": "Simpan",
+
+"cancel": "Batal",
+
+"ok": "oke",
+
+"done": "Selesai",
+
+"closeButtonLabel": "Tutup dialog",
+
+"windowButtonLabel": "Pilihan jendela",
+
+"windowMove": "Pindahkan",
+
+"windowMoveAlert": "Seret atau gunakan kunci panah untuk memindahkan jendela; Tekan Enter jika selesai",
+
+"windowResize": "Ubah ukuran",
+
+"windowResizeHeading": "Ubah Ukuran Jendela",
+
+"windowResizeAlert": "Ukuran jendela telah diubah.",
+
+"windowClose": "Tutup",
+
+"width": "Lebar",
+
+"height": "Tinggi",
+
+"windowSendBack": "Kirim ke Belakang",
+
+"windowSendBackAlert": "Jendela ini sekarang berada di belakang objek lain pada halaman.",
+
+"windowBringTop": "Bawa ke Depan",
+
+"windowBringTopAlert": "Jendela ini sekarang berada di depan objek lain pada halaman.",
+
+"resultsSummary1": "Anda mencari:",
+
+"resultsSummary2": "Ditemukan",
+
+"resultsSummary3": "hal yang sesuai.",
+
+"resultsSummary4": "Klik waktu terkait untuk memulai video dari titik tersebut.",
+
+"noResultsFound": "Tidak ada hasil yang ditemukan.",
+
+"searchButtonLabel": "Diputar pada",
+
+"hour": "jam",
+
+"minute": "menit",
+
+"second": "detik",
+
+"hours": "jam",
+
+"minutes": "menit",
+
+"seconds": "detik",
+
+"enableKeyboardShortcuts": "Aktifkan pintasan kibor"
+
+};
