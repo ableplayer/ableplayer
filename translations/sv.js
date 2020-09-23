b7328f9ef2e13e1581be4616713a17c76b5b232d
Stefan Wallin <github@stefan-wallin.se>
2020-09-23 19:33:22 +0200

    Create sv.js

---
+++ translations/sv.js
@@ -0,0 +1,337 @@
+var en = {
+
+"audio": "audio",
+
+"video": "video",
+
+"playerHeading": "Media player",
+
+"faster": "Faster",
+
+"slower": "Slower",
+
+"play": "Play",
+
+"pause": "Pause",
+
+"stop": "Stop",
+
+"restart": "Restart",
+
+"prevChapter": "Previous chapter",
+
+"nextChapter": "Next chapter",
+
+"prevTrack": "Previous track",
+
+"nextTrack": "Next track",
+
+"rewind": "Rewind",
+
+"forward": "Forward",
+
+"captions": "Captions",
+
+"showCaptions": "Show captions",
+
+"hideCaptions": "Hide captions",
+
+"captionsOff": "Captions off",
+
+"showTranscript": "Show transcript",
+
+"hideTranscript": "Hide transcript",
+
+"turnOnDescriptions": "Turn on descriptions",
+
+"turnOffDescriptions": "Turn off descriptions",
+
+"chapters": "Chapters",
+
+"newChapter": "New chapter",
+
+"language": "Language",
+
+"sign": "Sign language",
+
+"showSign": "Show sign language",
+
+"hideSign": "Hide sign language",
+
+"seekbarLabel": "timeline",
+
+"mute": "Mute",
+
+"unmute": "Unmute",
+
+"volume": "Volume",
+
+"volumeHelp": "Click to access volume slider",
+
+"volumeUpDown": "Volume up down",
+
+"volumeSliderClosed": "Volume slider closed",
+
+"preferences": "Preferences",
+
+"enterFullScreen": "Enter full screen",
+
+"exitFullScreen": "Exit full screen",
+
+"fullScreen": "Full screen",
+
+"speed": "Speed",
+
+"and": "and",
+
+"or": "or",
+
+"spacebar": "spacebar",
+
+"transcriptTitle": "Transcript",
+
+"lyricsTitle": "Lyrics",
+
+"autoScroll": "Auto scroll",
+
+"unknown": "Unknown",
+
+"statusPlaying": "Playing",
+
+"statusPaused": "Paused",
+
+"statusStopped": "Stopped",
+
+"statusWaiting": "Waiting",
+
+"statusBuffering": "Buffering",
+
+"statusUsingDesc": "Using described version",
+
+"statusLoadingDesc": "Loading described version",
+
+"statusUsingNoDesc": "Using non-described version",
+
+"statusLoadingNoDesc": "Loading non-described version",
+
+"statusLoadingNext": "Loading next track",
+
+"statusEnd": "End of track",
+
+"selectedTrack": "Selected Track",
+
+"alertDescribedVersion": "Using the audio described version of this video",
+
+"alertNonDescribedVersion": "Using the non-described version of this video",
+
+"fallbackError1": "Sorry, your browser is unable to play this",
+
+"fallbackError2": "The following browsers are known to work with this media player",
+
+"orHigher": "or higher",
+
+"prefMenuCaptions": "Captions",
+
+"prefMenuDescriptions": "Descriptions",
+
+"prefMenuKeyboard": "Keyboard",
+
+"prefMenuTranscript": "Transcript",
+
+"prefTitleCaptions": "Captions Preferences",
+
+"prefTitleDescriptions": "Audio Description Preferences",
+
+"prefTitleKeyboard": "Keyboard Preferences",
+
+"prefTitleTranscript": "Transcript Preferences",
+
+"prefIntroCaptions": "The following preferences control how captions are displayed.",
+
+"prefIntroDescription1": "This media player supports audio description in two ways: ",
+
+"prefIntroDescription2": "The current video has ",
+
+"prefIntroDescriptionNone": "The current video has no audio description in either format.",
+
+"prefIntroDescription3": "Use the following form to set your preferences related to text-based audio description.",
+
+"prefIntroDescription4": "After you save your settings, audio description can be toggled on/off using the Description button.",
+
+"prefIntroKeyboard1": "The media player on this web page can be operated from anywhere on the page using keyboard shortcuts (see below for a list).",
+
+"prefIntroKeyboard2": "Modifier keys (Shift, Alt, and Control) can be assigned below.",
+
+"prefIntroKeyboard3": "NOTE: Some key combinations might conflict with keys used by your browser and/or other software applications. Try various combinations of modifier keys to find one that works for you.",
+
+"prefIntroTranscript": "The following preferences affect the interactive transcript.",
+
+"prefCookieWarning": "Saving your preferences requires cookies.",
+
+"prefHeadingKeyboard1": "Modifier keys used for shortcuts",
+
+"prefHeadingKeyboard2": "Current keyboard shortcuts",
+
+"prefHeadingDescription": "Audio description",
+
+"prefHeadingTextDescription": "Text-based audio description",
+
+"prefHeadingCaptions": "Captions",
+
+"prefHeadingTranscript": "Interactive Transcript",
+
+"prefAltKey": "Alt",
+
+"prefCtrlKey": "Control",
+
+"prefShiftKey": "Shift",
+
+"escapeKey": "Escape",
+
+"escapeKeyFunction": "Close current dialog or popup menu",
+
+"prefDescFormat": "Preferred format",
+
+"prefDescFormatHelp": "If both formats are avaialable, only one will be used.",
+
+"prefDescFormatOption1": "alternative described version of video",
+
+"prefDescFormatOption1b": "an alternative described version",
+
+"prefDescFormatOption2": "text-based description, announced by screen reader",
+
+"prefDescFormatOption2b": "text-based description",
+
+"prefDescPause": "Automatically pause video when description starts",
+
+"prefVisibleDesc": "Make description visible",
+
+"prefHighlight": "Highlight transcript as media plays",
+
+"prefTabbable": "Keyboard-enable transcript",
+
+"prefCaptionsFont": "Font",
+
+"prefCaptionsColor": "Text Color",
+
+"prefCaptionsBGColor": "Background",
+
+"prefCaptionsSize": "Font Size",
+
+"prefCaptionsOpacity": "Opacity",
+
+"prefCaptionsStyle": "Style",
+
+"serif": "serif",
+
+"sans": "sans-serif",
+
+"cursive": "cursive",
+
+"fantasy": "fantasy",
+
+"monospace": "monospace",
+
+"white": "white",
+
+"yellow": "yellow",
+
+"green": "green",
+
+"cyan": "cyan",
+
+"blue": "blue",
+
+"magenta": "magenta",
+
+"red": "red",
+
+"black": "black",
+
+"transparent": "transparent",
+
+"solid": "solid",
+
+"captionsStylePopOn": "Pop-on",
+
+"captionsStyleRollUp": "Roll-up",
+
+"prefCaptionsPosition": "Position",
+
+"captionsPositionOverlay": "Overlay",
+
+"captionsPositionBelow": "Below video",
+
+"sampleCaptionText": "Sample caption text",
+
+"prefSuccess": "Your changes have been saved.",
+
+"prefNoChange": "You didn't make any changes.",
+
+"help": "Help",
+
+"helpTitle": "Help",
+
+"save": "Save",
+
+"cancel": "Cancel",
+
+"ok": "ok",
+
+"done": "Done",
+
+"closeButtonLabel": "Close dialog",
+
+"windowButtonLabel": "Window options",
+
+"windowMove": "Move",
+
+"windowMoveAlert": "Drag or use arrow keys to move the window; Enter to stop",
+
+"windowResize": "Resize",
+
+"windowResizeHeading": "Resize Window",
+
+"windowResizeAlert": "The window has been resized.",
+
+"windowClose": "Close",
+
+"width": "Width",
+
+"height": "Height",
+
+"windowSendBack": "Send to back",
+
+"windowSendBackAlert": "This window is now behind other objects on the page.",
+
+"windowBringTop": "Bring to front",
+
+"windowBringTopAlert": "This window is now in front of other objects on the page.",
+
+"resultsSummary1": "You searched for:",
+
+"resultsSummary2": "Found",
+
+"resultsSummary3": "matching items.",
+
+"resultsSummary4": "Click the time associated with any item to play the video from that point.",
+
+"noResultsFound": "No results found.",
+
+"searchButtonLabel": "Play at",
+
+"hour": "hour",
+
+"minute": "minute",
+
+"second": "second",
+
+"hours": "hours",
+
+"minutes": "minutes",
+
+"seconds": "seconds",
+
+"enableKeyboardShortcuts": "Enable keyboard shortcuts"
+
+};
