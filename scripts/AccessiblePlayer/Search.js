7c021b0c6a9fe94a96650e7345cda21b196841f1
monthebrice2000 <monthedjeumoubrice2000@gmail.com>
2022-05-16 22:57:22 +0200

    Complete refactoring of Search Class

---
+++ scripts/AccessiblePlayer/Search.js
@@ -0,0 +1,164 @@
+export default class Search{
+
+    setAblePlayer( ablePlayer ){
+        this.ablePlayer = ablePlayer;
+    }
+
+    // search VTT file for all instances of searchTerms
+    showSearchResults$() {
+
+        // search VTT file for all instances of searchTerms
+        // Currently just supports search terms separated with one or more spaces
+
+        // TODO: Add support for more robust search syntax:
+        // Search terms wrapped in quotation marks ("") must occur exactly as they appear in the quotes
+        // Search terms with an attached minus sign (e.g., -term) are to be excluded from results
+        // Boolean AND/OR operators
+        // ALSO: Add localization support
+
+        var thisObj = this;
+
+        if (this.ablePlayer.searchDiv && this.ablePlayer.searchString) {
+            if ($('#' + this.ablePlayer.SearchDiv)) {
+                var resultsArray = this.searchFor(this.ablePlayer.searchString);
+                if (resultsArray.length > 0) {
+                    var resultsSummary = $('<p>', {
+                        'class': 'able-search-results-summary'
+                    });
+                    var resultsSummaryText = 'Found <strong>' + resultsArray.length + '</strong> matching items. ';
+                    resultsSummaryText += 'Click the time associated with any item ';
+                    resultsSummaryText += 'to play the video from that point.';
+                    resultsSummary.html(resultsSummaryText);
+                    var resultsList = $('<ul>');
+                    for (var i = 0; i < resultsArray.length; i++) {
+                        var resultsItem = $('<li>', {});
+                        var itemStartTime = this.secondsToTime(resultsArray[i]['start']);
+                        var itemStartSpan = $('<span>', {
+                            'class': 'able-search-results-time',
+                            'data-start': resultsArray[i]['start'],
+                            'title': itemStartTime['title'],
+                            'tabindex': '0'
+                        });
+                        itemStartSpan.text(itemStartTime['value']);
+                        // add a listener for clisk on itemStart
+                        itemStartSpan.click(function (event) {
+                            var spanStart = parseFloat($(this).attr('data-start'));
+                            // Add a tiny amount so that we're inside the span.
+                            spanStart += .01;
+                            thisObj.ablePlayer.seeking = true;
+                            thisObj.ablePlayer.control.seekTo(spanStart);
+                        });
+
+                        var itemText = $('<span>', {
+                            'class': 'able-search-result-text'
+                        })
+                        itemText.html('...' + resultsArray[i]['caption'] + '...');
+                        resultsItem.append(itemStartSpan, itemText);
+                        resultsList.append(resultsItem);
+                    }
+                    $('#' + this.ablePlayer.searchDiv).append(resultsSummary, resultsList);
+                } else {
+                    var noResults = $('<p>').text('No results found.');
+                    $('#' + this.ablePlayer.searchDiv).append(noResults);
+                }
+            }
+        }
+    };
+
+    // return chronological array of caption cues that match searchTerms
+    searchFor (searchString, ignoreCaps) {
+        var captionLang, captions, results, caption, c, i, j;
+
+        // split searchTerms into an array
+        var searchTerms = searchString.split(' ');
+        if (this.ablePlayer.captions.length > 0) {
+            captionLang = this.ablePlayer.captions[0].language; // in case it's needed later
+            captions = this.ablePlayer.captions[0].cues;
+            if (captions.length > 0) {
+                var results = [];
+                c = 0;
+                for (i = 0; i < captions.length; i++) {
+                    if ($.inArray(captions[i].components.children[0]['type'], ['string', 'i', 'b', 'u', 'v', 'c']) !== -1) {
+                        caption = this.ablePlayer.caption.flattenCueForCaption(captions[i]);
+                        for (j = 0; j < searchTerms.length; j++) {
+                            if (caption.indexOf(searchTerms[j]) !== -1) {
+                                results[c] = [];
+                                results[c]['start'] = captions[i].start;
+                                results[c]['caption'] = this.highlightSearchTerm(searchTerms, j, caption);
+                                c++;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return results;
+    };
+
+    // highlight ALL found searchTerms in the current resultString
+    highlightSearchTerm (searchTerms, resultString) {
+        var i, searchTerm, termIndex, termLength, str1, str2, str3;
+
+        for (i = index; i < searchTerms.length; i++) {
+
+            searchTerm = searchTerms[i];
+            termIndex = resultString.indexOf(searchTerm);
+            if (termIndex !== -1) {
+                termLength = searchTerm.length;
+                if (termLength > 0) {
+                    str1 = resultString.substring(0, termIndex);
+                    str2 = '<span class="able-search-term">' + searchTerm + '</span>';
+                    str3 = resultString.substring(termIndex + termLength);
+                    resultString = str1 + str2 + str3;
+                } else {
+                    str1 = '<span class="able-search-term">' + searchTerm + '</span>';
+                    str2 = resultString.substring(termIndex + termLength);
+                    resultString = str1 + str2;
+                }
+            }
+        }
+        return resultString;
+    };
+
+    // return an array of totalSeconds converted into two formats
+    // time['value'] = HH:MM:SS with hours dropped if there are none
+    // time['title'] = a speakable rendering, so speech rec users can easily speak the link
+    secondsToTime (totalSeconds) {
+        var totalSeconds = Math.floor(totalSeconds);
+
+        var hours = parseInt(totalSeconds / 3600, 10) % 24;
+        var minutes = parseInt(totalSeconds / 60, 10) % 60;
+        var seconds = totalSeconds % 60;
+        var value = '';
+        var title = '';
+        if (hours > 0) {
+            value += hours + ':';
+            title + hours + ' hours ';
+        }
+        if (minutes < 10) {
+            value += '0' + minutes + ':';
+            if (minutes > 0) {
+                title += minutes + ' minutes ';
+            }
+        } else {
+            value += minutes + ':';
+            title += minutes + ' minutes ';
+        }
+        if (seconds < 10) {
+            value += '0' + seconds;
+            if (seconds > 0) {
+                title += seconds + ' seconds ';
+            }
+        } else {
+            value += seconds;
+            title += seconds + ' seconds ';
+        }
+        var time = [];
+        time['value'] = value;
+        time['title'] = title;
+        return time;
+    };
+
+}
