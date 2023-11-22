5883ff2150584c34a17ff6e32e3a41e78cb6eb7e
Fri <fri@Fris-MacBook-Air.local>
2023-11-22 14:26:12 +0800

    edit translation to better represent the contexts

--- translations/id.js
+++ translations/id.js
@@ -1,355 +1,531 @@
 {
 
+// "audio": "audio",
 "audio": "audio",
 
+// "video": "video",
 "video": "video",
 
+// "playerHeading": "Media player",
 "playerHeading": "Pemutar media",
 
-"faster": "Lebih cepat",
+// "faster": "Faster",
+"faster": "Percepat",
 
-"slower": "Lebih lambat",
+// "slower": "Slower",
+"slower": "Perlambat",
 
-"play": "Putar",
+// "play": "Play",
+"play": "Mulai",
 
+// "pause": "Pause",
 "pause": "Jeda",
 
-"stop": "Hentikan",
+// "stop": "Stop",
+"stop": "Berhenti",
 
-"restart": "Ulang",
+// "restart": "Restart",
+"restart": "Ulangi",
 
+// "prevChapter": "Previous chapter",
 "prevChapter": "Bagian sebelumnya",
 
+// "nextChapter": "Next chapter",
 "nextChapter": "Bagian berikutnya",
 
+// "prevTrack": "Previous track",
 "prevTrack": "Trek sebelumnya",
 
+// "nextTrack": "Next track",
 "nextTrack": "Trek berikutnya",
 
+// "rewind": "Rewind",
 "rewind": "Mundur",
 
+// "forward": "Forward",
 "forward": "Maju",
 
-"captions": "Teks",
+// "captions": "Captions",
+"captions": "Takarir",
 
-"showCaptions": "Tampilkan teks",
+// "showCaptions": "Show captions",
+"showCaptions": "Tampilkan takarir",
 
-"hideCaptions": "Sembunyikan teks",
+// "hideCaptions": "Hide captions",
+"hideCaptions": "Sembunyikan takarir",
 
-"captionsOff": "Matikan teks",
+// "captionsOff": "Captions off",
+"captionsOff": "Tanpa takarir",
 
+// "showTranscript": "Show transcript",
 "showTranscript": "Tampilkan transkripsi",
 
+// "hideTranscript": "Hide transcript",
 "hideTranscript": "Sembunyikan transkripsi",
 
+// "turnOnDescriptions": "Turn on descriptions",
 "turnOnDescriptions": "Nyalakan deskripsi",
 
+// "turnOffDescriptions": "Turn off descriptions",
 "turnOffDescriptions": "Matikan deskripsi",
 
+// "chapters": "Chapters",
 "chapters": "Bagian",
 
+// "newChapter": "New chapter",
 "newChapter": "Bagian baru",
 
+// "language": "Language",
 "language": "Bahasa",
 
+// "sign": "Sign language",
 "sign": "Bahasa isyarat",
 
+// "showSign": "Show sign language",
 "showSign": "Tampilkan bahasa isyarat",
 
+// "hideSign": "Hide sign language",
 "hideSign": "Sembunyikan bahasa isyarat",
 
-"seekbarLabel": "Lini masa",
+// "seekbarLabel": "timeline",
+"seekbarLabel": "lini masa",
 
-"mute": "Bisukan",
+// "mute": "Mute",
+"mute": "Matikan Suara",
 
-"unmute": "Suarakan",
+// "unmute": "Unmute",
+"unmute": "Nyalakan Suara",
 
+// "volume": "Volume",
 "volume": "Volume",
 
+// "volumeHelp": "Click to access volume slider",
 "volumeHelp": "Klik untuk mengakses pengatur volume",
 
-"volumeUpDown": "Perbesar volume",
+// "volumeUpDown": "Volume up down",
+"volumeUpDown": "Naik turun volume",
 
-"volumeSliderClosed": "Tutup pengatur volume",
+// "volumeSliderClosed": "Volume slider closed",
+"volumeSliderClosed": "Pengatur volume ditutup",
 
+// "preferences": "Preferences",
 "preferences": "Preferensi",
 
+// "enterFullscreen": "Enter full screen",
 "enterFullscreen": "Masuk ke mode layar penuh",
 
+// "exitFullscreen": "Exit full screen",
 "exitFullscreen": "Keluar dari mode layar penuh",
 
+// "fullscreen": "Full screen",
 "fullscreen": "Layar penuh",
 
+// "speed": "Speed",
 "speed": "Kecepatan",
 
+// "and": "and",
 "and": "dan",
 
+// "or": "or",
 "or": "atau",
 
-"spacebar": "kunci spasi",
+// "spacebar": "spacebar",
+"spacebar": "tombol spasi",
 
+// "transcriptTitle": "Transcript",
 "transcriptTitle": "Transkripsi",
 
+// "lyricsTitle": "Lyrics",
 "lyricsTitle": "Lirik",
 
-"autoScroll": "Otomatis bergulir",
+// "autoScroll": "Auto scroll",
+"autoScroll": "Gulir otomatis",
 
-"unknown": "Tidak diketahui",
+// "unknown": "Unknown",
+"unknown": "Tak diketahui",
 
-"statusPlaying": "Diputar",
+// "statusPlaying": "Playing",
+"statusPlaying": "Memutar",
 
+// "statusPaused": "Paused",
 "statusPaused": "Dijedakan",
 
+// "statusStopped": "Stopped",
 "statusStopped": "Dihentikan",
 
+// "statusWaiting": "Waiting",
 "statusWaiting": "Menunggu",
 
-"statusBuffering": "Sedang bufer",
+// "statusBuffering": "Buffering",
+"statusBuffering": "Sedang Bufer",
 
-"statusUsingDesc": "Menggunakan versi yang tertera",
+// "statusUsingDesc": "Using described version",
+"statusUsingDesc": "Menggunakan versi yang dideskripsikan",
 
-"statusLoadingDesc": "Memuat versi yang tertera",
+// "statusLoadingDesc": "Loading described version",
+"statusLoadingDesc": "Memuat versi yang dideskripsikan",
 
-"statusUsingNoDesc": "Menggunakan versi yang bukan tertera",
+// "statusUsingNoDesc": "Using non-described version",
+"statusUsingNoDesc": "Menggunkan versi yang tak dideskripsikan",
 
-"statusLoadingNoDesc": "Memuat versi yang bukan tertera",
+// "statusLoadingNoDesc": "Loading non-described version",
+"statusLoadingNoDesc": "Memuat versi yang tak dideskripsikan",
 
+// "statusLoadingNext": "Loading next track",
 "statusLoadingNext": "Memuat trek berikutnya",
 
-"statusEnd": "Akhir trek",
+// "statusEnd": "End of track",
+"statusEnd": "Akhir dari trek",
 
-"selectedTrack": "Trek pilihan",
+// "selectedTrack": "Selected Track",
+"selectedTrack": "Trek Pilihan",
 
-"alertDescribedVersion": "Menggunakan versi audio yang tertera dari video ini",
+// "alertDescribedVersion": "Using the audio described version of this video",
+"alertDescribedVersion": "Menggunakan versi dengan deskripsi audio pada video ini",
 
-"alertNonDescribedVersion": "Mnggunakan versi yang bukan tertera dari video ini",
+// "alertNonDescribedVersion": "Using the non-described version of this video",
+"alertNonDescribedVersion": "Menggunakan versi tanpa deskripsi pada video ini",
 
-"fallbackError1": "Maaf, browser Anda tidak bisa memainkan ini",
+// "fallbackError1": "Sorry, your browser is unable to play this",
+"fallbackError1": "Maaf, peramban Anda tidak bisa memutar ini",
 
-"fallbackError2": "Browser berikut diketahui bisa menggunakan pemutar video ini",
+// "fallbackError2": "The following browsers are known to work with this media player",
+"fallbackError2": "Peramban berikut diketahui tidak dapat menggunakan pemutar media ini",
 
+// "orHigher": "or higher",
 "orHigher": "atau lebih tinggi",
 
-"prefMenuCaptions": "Teks",
+// "prefMenuCaptions": "Captions",
+"prefMenuCaptions": "Takarir",
 
+// "prefMenuDescriptions": "Descriptions",
 "prefMenuDescriptions": "Deskripsi",
 
+// "prefMenuKeyboard": "Keyboard",
 "prefMenuKeyboard": "Kibor",
 
+// "prefMenuTranscript": "Transcript",
 "prefMenuTranscript": "Transkripsi",
 
-"prefTitleCaptions": "Preferensi Teks",
+// "prefTitleCaptions": "Captions Preferences",
+"prefTitleCaptions": "Preferensi Takarir",
 
+// "prefTitleDescriptions": "Audio Description Preferences",
 "prefTitleDescriptions": "Preferensi Deskripsi Audio",
 
+// "prefTitleKeyboard": "Keyboard Preferences",
 "prefTitleKeyboard": "Preferensi Kibor",
 
-"prefTitleTranscript": "Preferensi transkripsi",
+// "prefTitleTranscript": "Transcript Preferences",
+"prefTitleTranscript": "Preferensi Transkripsi",
 
-"prefIntroCaptions": "Preferensi berikut mengontrol cara menampilkan teks.",
+// "prefIntroCaptions": "The following preferences control how captions are displayed.",
+"prefIntroCaptions": "Preferensi berikut mengontrol cara takarir ditampilkan.",
 
+// // "prefIntroDescription1": "This media player supports audio description in two ways: ",
 "prefIntroDescription1": "Pemutar media ini mendukung deskripsi audio dengan dua cara: ",
 
+// "prefIntroDescription2": "The current video has ",
 "prefIntroDescription2": "Video ini memiliki ",
 
-"prefIntroDescriptionNone": "Video ini tidak memiliki deskripsi audio dalam format apa pun.",
+// "prefIntroDescriptionNone": "The current video has no audio description in either format.",
+"prefIntroDescriptionNone": "Video ini tidak memiliki deskripsi audio dalam format mana pun.",
 
-"prefIntroDescription3": "Gunakan formulir berikut untuk memasang preferensi terkait deskripsi audio berbasis teks.",
+// "prefIntroDescription3": "Use the following form to set your preferences related to text-based audio description.",
+"prefIntroDescription3": "Gunakan isian berikut untuk memasang preferensi terkait deskripsi audio berbasis teks.",
 
-"prefIntroDescription4": "Setelah menyimpan pengaturan Anda, deskripsi audio dapat dinyalakan/dimatikan melalui tombol Deskripsi.",
+// "prefIntroDescription4": "After you save your settings, audio description can be toggled on/off using the Description button.",
+"prefIntroDescription4": "Setelah Anda menyimpan pengaturan, deskripsi audio dapat dinyala/matikan melalui tombol Deskripsi.",
 
-"prefIntroKeyboard1": "Pemutar media dalam halaman ini dapat dioperasikan dari bagian mana pun di halaman dengan menggunakan pintasan kibor (lihat daftar di bawah).",
+// "prefIntroKeyboard1": "The media player on this web page can be operated from anywhere on the page using keyboard shortcuts (see below for a list).",
+"prefIntroKeyboard1": "Pemutar media dalam halaman ini dapat dioperasikan dari bagian mana pun pada halaman dengan menggunakan pintasan kibor (lihat daftar di bawah).",
 
-"prefIntroKeyboard2": "Kunci pengubah (Shift, Alt, Control) dapat diperuntukkan di bawah.",
+// "prefIntroKeyboard2": "Modifier keys (Shift, Alt, and Control) can be assigned below.",
+"prefIntroKeyboard2": "Fungsi tombol kombinasi (Shift, Alt, Control) dapat dipasangkan di bawah ini.",
 
-"prefIntroKeyboard3": "CATATAN: Beberapa kombinasi kunci mungkin akan konflik dengan yang digunakan oleh browser Anda dan/atau aplikasi perangkat lunak lainnya. Cobalah beragam kombinasi untuk menemukan yang sesuai untuk Anda.",
+// // "prefIntroKeyboard3": "NOTE: Some key combinations might conflict with keys used by your browser and/or other software applications. Try various combinations of modifier keys to find one that works for you.",
+"prefIntroKeyboard3": "CATATAN: Beberapa kombinasi tombol mungkin akan bersinggungan dengan yang digunakan oleh browser dan/atau aplikasi perangkat lunak lainnya. Cobalah berbagai kombinasi untuk menemukan kombinasi yang bekerja untuk Anda.",
 
-"prefIntroTranscript": "Preferensi berikut mempengaruhi transkripsi interaktif.",
+// "prefIntroTranscript": "The following preferences affect the interactive transcript.",
+"prefIntroTranscript": "Preferensi berikut akan memengaruhi transkripsi interaktif.",
 
-"prefCookieWarning": "Untuk menyimpan preferesi dibutuhkan kukis.",
+// "prefCookieWarning": "Saving your preferences requires cookies.",
+"prefCookieWarning": "Menyimpan preferesi membutuhkan kuki.",
 
-"prefHeadingKeyboard1": "Kunci pengubah untuk pintasan",
+// "prefHeadingKeyboard1": "Modifier keys used for shortcuts",
+"prefHeadingKeyboard1": "Tombol kombinasi yang digunakan untuk pintasan",
 
-"prefHeadingKeyboard2": "Kunci pintasan saat ini",
+// "prefHeadingKeyboard2": "Current keyboard shortcuts",
+"prefHeadingKeyboard2": "Pintasan kibor saat ini",
 
+// "prefHeadingDescription": "Audio description",
 "prefHeadingDescription": "Deskripsi audio",
 
+// "prefHeadingTextDescription": "Text-based audio description",
 "prefHeadingTextDescription": "Deskripsi audio berbasis teks",
 
-"prefHeadingCaptions": "Teks",
+// "prefHeadingCaptions": "Captions",
+"prefHeadingCaptions": "Takarir",
 
+// "prefHeadingTranscript": "Interactive Transcript",
 "prefHeadingTranscript": "Transkripsi Interaktif",
 
+// "prefAltKey": "Alt",
 "prefAltKey": "Alt",
 
+// "prefCtrlKey": "Control",
 "prefCtrlKey": "Control",
 
+// "prefShiftKey": "Shift",
 "prefShiftKey": "Shift",
 
+// "escapeKey": "Escape",
 "escapeKey": "Escape",
 
-"escapeKeyFunction": "Tutup menu dialog atau popup ini",
+// "escapeKeyFunction": "Close current dialog or popup menu",
+"escapeKeyFunction": "Tutup dialog atau menu sembulan yang tampil",
 
-"prefDescFormat": "Format yang disukai",
+// "prefDescFormat": "Preferred format",
+"prefDescFormat": "Format yang dipilih",
 
-"prefDescFormatHelp": "Jika kedua format tersedia, hanya satu yang akan digunakan.",
+// "prefDescFormatHelp": "If both formats are avaialable, only one will be used.",
+"prefDescFormatHelp": "Jika kedua format tersedia, salah satu akan digunakan.",
 
-"prefDescFormatOption1": "versi alternatif video yang tertera",
+// "prefDescFormatOption1": "alternative described version of video",
+"prefDescFormatOption1": "versi alternatif video dengan deskripsi",
 
-"prefDescFormatOption1b": "versi alternatif yang tertera",
+// "prefDescFormatOption1b": "an alternative described version",
+"prefDescFormatOption1b": "versi alternatif berdeskripsi"
 
+// "prefDescFormatOption2": "text-based description, announced by screen reader",
 "prefDescFormatOption2": "deskripsi berbasis teks, dibacakan oleh pembaca layar",
 
+// "prefDescFormatOption2b": "text-based description",
 "prefDescFormatOption2b": "deskripsi berbasis teks",
 
+// "prefDescPause": "Automatically pause video when description starts",
 "prefDescPause": "Otomatis jedakan video ketika deskripsi dimulai",
 
+// "prefDescVisible": "Make description visible",
+"prefDescVisible": "Tampilkan deskripsi",
+
+// "prefDescVoice": "Voice",
 "prefDescVoice": "Suara",
 
+// "prefDescRate": "Rate",
 "prefDescRate": "Kecepatan",
 
+// "prefDescPitch": "Pitch",
 "prefDescPitch": "Titinada",
 
+// "prefDescPitch1": "Very low",
 "prefDescPitch1": "Sangat rendah",
 
+// "prefDescPitch2": "Low",
 "prefDescPitch2": "Rendah",
 
-"prefDescPitch3": "Default",
+// "prefDescPitch3": "Default",
+"prefDescPitch3": "Bawaan",
 
+// "prefDescPitch4": "High",
 "prefDescPitch4": "Tinggi",
 
+// "prefDescPitch5": "Very high",
 "prefDescPitch5": "Sangat tinggi",
 
-"sampleDescriptionText": "Sesuaikan pengaturan untuk mendengarkan teks contoh ini.",
+// "sampleDescriptionText": "Adjust settings to hear this sample text.",
+"sampleDescriptionText": "Sesuaikan pengaturan untuk mendengarkan teks ini.",
 
-"prefDescVisible": "Menampakkan deskripsi",
+// "prefHighlight": "Highlight transcript as media plays",
+"prefHighlight": "Soroti transkripsi yang sesuai saat media diputar",
 
-"prefHighlight": "Sorot transkripsi saat media diputar",
-
+// "prefTabbable": "Keyboard-enable transcript",
 "prefTabbable": "Transkripsi dengan kontrol kibor",
 
+// "prefCaptionsFont": "Font",
 "prefCaptionsFont": "Jenis Huruf",
 
+// "prefCaptionsColor": "Text Color",
 "prefCaptionsColor": "Warna Teks",
 
+// "prefCaptionsBGColor": "Background",
 "prefCaptionsBGColor": "Warna Latar",
 
-"prefCaptionsSize": "Ukuran Teks",
+// "prefCaptionsSize": "Font Size",
+"prefCaptionsSize": "Ukuran Huruf",
 
+// "prefCaptionsOpacity": "Opacity",
 "prefCaptionsOpacity": "Opasitas",
 
+// "prefCaptionsStyle": "Style",
 "prefCaptionsStyle": "Gaya",
 
+// "serif": "serif",
 "serif": "serif",
 
+// "sans": "sans-serif",
 "sans": "sans-serif",
 
-"cursive": "huruf sambung",
+// "cursive": "cursive",
+"cursive": "kursif",
 
+// "fantasy": "fantasy",
 "fantasy": "fantasi",
 
-"monospace": "monospace",
+// "monospace": "monospace",
+"monospace": "monospasi",
 
+// "white": "white",
 "white": "putih",
 
+// "yellow": "yellow",
 "yellow": "kuning",
 
+// "green": "green",
 "green": "hijau",
 
+// "cyan": "cyan",
 "cyan": "sian",
 
+// "blue": "blue",
 "blue": "biru",
 
+// "magenta": "magenta",
 "magenta": "magenta",
 
+// "red": "red",
 "red": "merah",
 
+// "black": "black",
 "black": "hitam",
 
+// "transparent": "transparent",
 "transparent": "transparan",
 
+// "solid": "solid",
 "solid": "solid",
 
+// "captionsStylePopOn": "Pop-on",
 "captionsStylePopOn": "Muncul",
 
-"captionsStyleRollUp": "Bergulir",
+// "captionsStyleRollUp": "Roll-up",
+"captionsStyleRollUp": "Bergulir naik",
 
+// "prefCaptionsPosition": "Position",
 "prefCaptionsPosition": "Posisi",
 
-"captionsPositionOverlay": "Melapisi video",
+// "captionsPositionOverlay": "Overlay",
+"captionsPositionOverlay": "Di atas video",
 
+// "captionsPositionBelow": "Below video",
 "captionsPositionBelow": "Di bawah video",
 
-"sampleCaptionText": "Contoh tampilan teks",
+// "sampleCaptionText": "Sample caption text",
+"sampleCaptionText": "Contoh teks takarir",
 
-"prefSuccess": "Perubahan yang Anda buat sudah disimpan.",
+// "prefSuccess": "Your changes have been saved.",
+"prefSuccess": "Perubahan telah disimpan.",
 
-"prefNoChange": "Anda tidak mengubah apa pun.",
+// "prefNoChange": "You didn't make any changes.",
+"prefNoChange": "Anda tidak melakukan perubahan apa pun.",
 
+// "help": "Help",
 "help": "Bantuan",
 
+// "helpTitle": "Help",
 "helpTitle": "Bantuan",
 
+// "save": "Save",
 "save": "Simpan",
 
+// "cancel": "Cancel",
 "cancel": "Batal",
 
+// "ok": "ok",
 "ok": "oke",
 
+// "done": "Done",
 "done": "Selesai",
 
+// "closeButtonLabel": "Close dialog",
 "closeButtonLabel": "Tutup dialog",
 
-"windowButtonLabel": "Pilihan jendela",
+// "windowButtonLabel": "Window options",
+"windowButtonLabel": "Opsi bingkai",
 
-"windowMove": "Pindahkan",
+// "windowMove": "Move",
+"windowMove": "Geser",
 
-"windowMoveAlert": "Seret atau gunakan kunci panah untuk memindahkan jendela; Tekan Enter jika selesai",
+// "windowMoveAlert": "Drag or use arrow keys to move the window; Enter to stop",
+"windowMoveAlert": "Seret atau gunakan tombol panah untuk menggeser jendela; Tekan Enter untuk berhenti",
 
+// "windowResize": "Resize",
 "windowResize": "Ubah ukuran",
 
+// "windowResizeHeading": "Resize Window",
 "windowResizeHeading": "Ubah Ukuran Jendela",
 
+// "windowResizeAlert": "The window has been resized.",
 "windowResizeAlert": "Ukuran jendela telah diubah.",
 
+// "windowClose": "Close",
 "windowClose": "Tutup",
 
+// "width": "Width",
 "width": "Lebar",
 
+// "height": "Height",
 "height": "Tinggi",
 
-"windowSendBack": "Kirim ke Belakang",
+// "windowSendBack": "Send to back",
+"windowSendBack": "Pindahkan ke belakang",
 
+// "windowSendBackAlert": "This window is now behind other objects on the page.",
 "windowSendBackAlert": "Jendela ini sekarang berada di belakang objek lain pada halaman.",
 
-"windowBringTop": "Bawa ke Depan",
+// "windowBringTop": "Bring to front",
+"windowBringTop": "Bawa ke depan",
 
+// "windowBringTopAlert": "This window is now in front of other objects on the page.",
 "windowBringTopAlert": "Jendela ini sekarang berada di depan objek lain pada halaman.",
 
-"resultsSummary1": "Anda mencari:",
+// // "resultsSummary1": "You searched for:",
+"resultsSummary1": "You searched for:",
 
+// "resultsSummary2": "Found",
 "resultsSummary2": "Ditemukan",
 
+// "resultsSummary3": "matching items.",
 "resultsSummary3": "hal yang sesuai.",
 
-"resultsSummary4": "Klik waktu terkait untuk memulai video dari titik tersebut.",
+// "resultsSummary4": "Click the time associated with any item to play the video from that point.",
+"resultsSummary4": "Klik waktu yang terkait dengan suatu hal untuk memutar video dari titik tersebut.",
 
+// "noResultsFound": "No results found.",
 "noResultsFound": "Tidak ada hasil yang ditemukan.",
 
+// "searchButtonLabel": "Play at",
 "searchButtonLabel": "Diputar pada",
 
+// "hour": "hour",
 "hour": "jam",
 
+// "minute": "minute",
 "minute": "menit",
 
+// "second": "second",
 "second": "detik",
 
+// "hours": "hours",
 "hours": "jam",
 
+// "minutes": "minutes",
 "minutes": "menit",
 
+// "seconds": "seconds",
 "seconds": "detik",
 
+// "enableKeyboardShortcuts": "Enable keyboard shortcuts"
 "enableKeyboardShortcuts": "Aktifkan pintasan kibor"
 
 }
