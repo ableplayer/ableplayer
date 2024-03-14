6c41745b7b122c2c7e019eaa53e4aba68c6a16ef
Stefan Wajda <audyt@lepszyweb.pl>
2024-03-14 16:27:21 +0100

    Add polish translation

---
+++ translations/pl.js
@@ -0,0 +1,355 @@
+{
+
+"audio": "dźwięk",
+
+"video": "wideo",
+
+"playerHeading": "Odtwarzacz mediów",
+
+"faster": "Szybciej",
+
+"slower": "Wolniej",
+
+"play": "Odtwarzaj",
+
+"pause": "Pauza",
+
+"stop": "Zatrzymaj",
+
+"restart": "Restart",
+
+"prevChapter": "Poprzedni rozdział",
+
+"nextChapter": "Następny rozdział",
+
+"prevTrack": "Poprzednia ścieżka",
+
+"nextTrack": "Następna ścieżka",
+
+"rewind": "Przewiń w tył",
+
+"forward": "Przewiń w przód",
+
+"captions": "Napisy",
+
+"showCaptions": "Pokaż napisy",
+
+"hideCaptions": "Ukryj napisy",
+
+"captionsOff": "Wyłącz napisy",
+
+"showTranscript": "Pokaż transkrypcję",
+
+"hideTranscript": "Ukryj transkrypcję",
+
+"turnOnDescriptions": "Włącz opis audio",
+
+"turnOffDescriptions": "Wyłącz opis audio",
+
+"chapters": "Rozdziały",
+
+"newChapter": "Nowy rozdział",
+
+"language": "Język",
+
+"sign": "Język migowy",
+
+"showSign": "Pokaż język migowy",
+
+"hideSign": "Ukryj język migowy",
+
+"seekbarLabel": "Linia czasu",
+
+"mute": "Wycisz",
+
+"unmute": "Pogłośnij",
+
+"volume": "Głośność",
+
+"volumeHelp": "Naciśnij, aby uzyskać dostęp do suwaka głośności",
+
+"volumeUpDown": "Głośniej/Ciszej",
+
+"volumeSliderClosed": "Suwak głośności zamknięto",
+
+"preferences": "Preferencje",
+
+"enterFullscreen": "Wejdź na pełny ekran",
+
+"exitFullscreen": "Wyjdź z pełnego ekranu",
+
+"fullscreen": "Pełny ekran",
+
+"speed": "Tempo",
+
+"and": "i",
+
+"or": "lub",
+
+"spacebar": "spacja",
+
+"transcriptTitle": "Transkrypcja",
+
+"lyricsTitle": "Tekst",
+
+"autoScroll": "Auto przewijanie",
+
+"unknown": "Nieznane",
+
+"statusPlaying": "Odtwarzanie",
+
+"statusPaused": "Wstrzymane",
+
+"statusStopped": "Zatrzymane",
+
+"statusWaiting": "Oczekiwanie",
+
+"statusBuffering": "Buforowanie",
+
+"statusUsingDesc": "Użyj wersji z opisem audio",
+
+"statusLoadingDesc": "Ładowanie wersji z opisem audio",
+
+"statusUsingNoDesc": "Użyj wersji bez opisu audio",
+
+ "statusLoadingNoDesc": "Ładowanie wersji bez opisu audio",
+
+"statusLoadingNext": "Ładowanie następnej ścieżki",
+
+"statusEnd": "Koniec ścieżki",
+
+"selectedTrack": "Wybrana ścieżka",
+
+"alertDescribedVersion": "Użyj wersji filmu z opisem audi",
+
+"alertNonDescribedVersion": "Użyj wersji filmu bez opisu audio",
+
+"fallbackError1": "Opss, Twoja przeglądarka nie może tego odtworzyć",
+
+"fallbackError2": "Następujące przeglądarki współpracują z tym odtwarzaczem multimedialnym",
+
+"orHigher": "lub wyższą",
+
+"prefMenuCaptions": "Napisy",
+
+"prefMenuDescriptions": "Audiodeskrypcja",
+
+"prefMenuKeyboard": "Klawiatura",
+
+"prefMenuTranscript": "Transkrypcja",
+
+"prefTitleCaptions": "Preferencje napisów",
+
+"prefTitleDescriptions": "Preferencje audiodeskrypcji",
+
+"prefTitleKeyboard": "Preferencje klawiatury",
+
+"prefTitleTranscript": "Preferencje transkrypcji",
+
+"prefIntroCaptions": "Poniższe preferencje kontrolują sposób wyświetlania napisów.",
+
+"prefIntroDescription1": "Ten odtwarzacz mediów obsługuje audiodeskrypcję na dwa sposoby: ",
+
+"prefIntroDescription2": "Bieżący film ma ",
+
+"prefIntroDescriptionNone": "Bieżący film nie ma audiodeskprycji.",
+
+"prefIntroDescription3": "Użyj formularza poniższej, aby określić ustawienia odtwarzania tekstowego opisu audio",
+
+"prefIntroDescription4": "Po zapisaniu ustawień opisu audio można włączać/wyłączać za pomocą przycisku Audiodeskrypcja.",
+
+"prefIntroKeyboard1": "Odtwarzaczem mediów na tej stronie można sterować z dowolnego miejsca na stronie za pomocą skrótów klawiaturowych (lista poniżej).",
+
+"prefIntroKeyboard2": "Klawisze modyfikujące (Shift, Alt i Control) można przypisać poniżej",
+
+"prefIntroKeyboard3": "UWAGA: Niektóre kombinacje klawiszy mogą kolidować z klawiszami używanymi przez przeglądarkę i/lub inne aplikacje. Wypróbuj różne kombinacje klawiszy modyfikujących, aby znaleźć taką, która będzie dla Ciebie odpowiednia.",
+
+"prefIntroTranscript": "Poniższe preferencje mają wpływ na interaktywną transkrypcję.",
+
+"prefCookieWarning": "Zapisanie preferencji wymaga plików cookie.",
+
+"prefHeadingKeyboard1": "Klawisze modyfikujące używane do skrótów",
+
+"prefHeadingKeyboard2": "Bieżące skróty klawiaturowe",
+
+"prefHeadingDescription": "Audiodeskrypcja",
+
+"prefHeadingTextDescription": "Tekst oparty na audiodeskrypcji",
+
+"prefHeadingCaptions": "Napisy",
+
+"prefHeadingTranscript": "Interaktywna transkrypcja",
+
+"prefAltKey": "Alt",
+
+"prefCtrlKey": "Control",
+
+"prefShiftKey": "Shift",
+
+"escapeKey": "Escape",
+
+"escapeKeyFunction": "Zamknij bieżące okno dialogowe lub menu podręczne",
+
+"prefDescFormat": "Preferowany format",
+
+"prefDescFormatHelp": "Jeśli dostępne są oba formaty, użyty zostanie tylko jeden z nich.",
+
+"prefDescFormatOption1": "alternatywna wersja wideo z opisem audio",
+
+"prefDescFormatOption1b": "alternatywna wersja opisu audio",
+
+"prefDescFormatOption2": "tekst opisu audio, ogłaszany przez czytnik ekranu",
+
+"prefDescFormatOption2b": "tekst opisu audio",
+
+"prefDescPause": "Automatycznie wstrzymuj wideo po rozpoczęciu audiodeskrypcji",
+
+"prefDescVisible": "Wyświetlaj opis audio",
+
+"prefDescVoice": "Głos",
+
+"prefDescRate": "Tempo",
+
+"prefDescPitch": "Wysokość",
+
+"prefDescPitch1": "Bardzo niska",
+
+"prefDescPitch2": "Niska",
+
+"prefDescPitch3": "Domyślna",
+
+"prefDescPitch4": "Wysoka",
+
+"prefDescPitch5": "Bardzo wysoko",
+
+"sampleDescriptionText": "Edytuj ustawienia, aby odczytać ten tekst na głos.",
+
+"prefHighlight": "Podświetl transkrypcję podczas odtwarzania mediów",
+
+"prefTabbable": "Włącz transkrypcję za pomocą klawiatury",
+
+"prefCaptionsFont": "Czcionka",
+
+"prefCaptionsColor": "Kolor tekstu",
+
+"prefCaptionsBGColor": "Kolor tła",
+
+"prefCaptionsSize": "Rozmiar czcionki",
+
+"prefCaptionsOpacity": "Krycie",
+
+"prefCaptionsStyle": "Styl",
+
+"serif": "szryfowa",
+
+"sans": "bezszeryfowa",
+
+"cursive": "pochyła",
+
+"fantasy": "fantazyjna",
+
+"monospace": "stała szerokość",
+
+"white": "biały",
+
+"yellow": "żółty",
+
+"green": "zielony",
+
+"cyan": "cyjan",
+
+"blue": "niebieski",
+
+"magenta": "magenta",
+
+"red": "czerwony",
+
+"black": "czarny",
+
+"transparent": "transparente",
+
+"solid": "solidne",
+
+"captionsStylePopOn": "Rozwiń",
+
+"captionsStyleRollUp": "Zwiń",
+
+"prefCaptionsPosition": "Pozycja",
+
+"captionsPositionOverlay": "Nakładka",
+
+"captionsPositionBelow": "Poniżej wideo",
+
+"sampleCaptionText": "Przykładowy tekst napisu",
+
+"prefSuccess": "Zmiany zostały zapisane.",
+
+"prefNoChange": "Nie wprowadzono żadnych zmian.",
+
+"help": "Pomoc",
+
+"helpTitle": "Pomoc",
+
+"save": "Zapisz",
+
+"cancel": "Anuluj",
+
+"ok": "Ok",
+
+"done": "Zrobone",
+
+"closeButtonLabel": "Zamknij okno",
+
+"windowButtonLabel": "Opcje okna",
+
+"windowMove": "Przenieś",
+
+"windowMoveAlert": "Przeciągnij lub użyj strzałek, aby przesunąć okno; Enter, aby zatrzymać",
+
+"windowResize": "Zmień rozmiar",
+
+"windowResizeHeading": "Zmień rozmiar okna",
+
+"windowResizeAlert": "Rozmiar okna zmieniono.",
+
+"windowClose": "Zamknij",
+
+"width": "Szerokość",
+
+"height": "Wysokość",
+
+"windowSendBack": "Przenieś do tyłu",
+
+"windowSendBackAlert": "To okno znajduje się teraz za innymi obiektami na stronie.",
+
+"windowBringTop": "Przenieś do przodu",
+
+"windowBringTopAlert": "To okno znajduje się teraz przed innymi obiektami na stronie.",
+
+"resultsSummary1": "Szukano:",
+
+"resultsSummary2": "Znaleziono",
+
+"resultsSummary3": "pasujące elementy.",
+
+"resultsSummary4": "Kliknij czas powiązany z dowolnym elementem, aby odtworzyć wideo od tego momentu.",
+
+"noResultsFound": "Nie znaleziono żadnych wyników",
+
+"searchButtonLabel": "Odtwarzaj od",
+
+"hour": "godzina",
+
+"minute": "minuta",
+
+"second": "sekunda",
+
+"hours": "godziny",
+
+"minutes": "minuty",
+
+"seconds": "sekundy",
+
+"enableKeyboardShortcuts": "Włącz skróty klawiaturowe"
+
+}
