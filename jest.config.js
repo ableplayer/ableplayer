bae0a660d9530be58831e1b4f858bec70ddff32f
Jeane Marty <jeanem@uw.edu>
2025-01-07 10:18:23 -0800

    fix security issue in the client-side search demo page

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,15 @@
 module.exports = {
-  preset: "jest-puppeteer",
-  testRegex: "./*\\.test\\.js$",
+  projects: [
+    {
+      displayName: "jsdom",
+      testEnvironment: "jest-environment-jsdom",
+      testMatch: ["**/__tests__/**/*.test.js"],
+      testPathIgnorePatterns: ["/node_modules/", "/__tests__/validate.test.js"],
+    },
+    {
+      displayName: "puppeteer",
+      preset: "jest-puppeteer",
+      testMatch: ["**/__tests__/validate.test.js"],
+    },
+  ],
 };
