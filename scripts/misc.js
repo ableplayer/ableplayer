fa3ae304f2e7258701ef1cf1ce4a2555cfbb94f3
Joe Dolson <design@joedolson.com>
2025-12-30 14:53:46 -0600

    Replace jQuery getScript
    Follow up to #709

--- scripts/misc.js
+++ scripts/misc.js
@@ -141,6 +141,26 @@
 	});
   }
 
+  AblePlayer.prototype.getScript = function( source, callback ) {
+	var script   = document.createElement('script');
+	var prior    = document.getElementsByTagName('script')[0];
+	script.async = 1;
+
+	script.onload = script.onreadystatechange = function( _, isAbort ) {
+		if ( isAbort || !script.readyState || /loaded|complete/.test(script.readyState) ) {
+			script.onload = script.onreadystatechange = null;
+			script        = undefined;
+
+			if ( !isAbort && callback ) {
+				setTimeout(callback, 0);
+			}
+		}
+	};
+
+	script.src = source;
+	prior.parentNode.insertBefore(script, prior);
+  }
+
   AblePlayer.prototype.hasAttr = function (object, attribute) {
     // surprisingly, there is no hasAttr() function in Jquery as of 3.2.1
     // return true if object has attribute; otherwise false
