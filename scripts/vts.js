0bf5390b36b226958c5c2f25774f32f45eeef9af
Joe Dolson <design@joedolson.com>
2026-01-12 11:08:49 -0600

    Two VTS translation strings operating on wrong object.

--- scripts/vts.js
+++ scripts/vts.js
@@ -139,7 +139,7 @@
 					e.stopPropagation();
 					if ($(this).attr('value') == 'save') {
 						// replace table with WebVTT output in textarea fields (for copying/pasting)
-						$(this).attr('value','cancel').text( this.translate( 'vtsReturn', 'Return to Editor' ) );
+						$(this).attr('value','cancel').text( thisObj.translate( 'vtsReturn', 'Return to Editor' ) );
 						$savedTable = $('#able-vts table');
 						$('#able-vts-instructions').hide();
 						$('#able-vts > fieldset').hide();
@@ -154,7 +154,7 @@
 						$('#able-vts > fieldset').show();
 						$('#able-vts').append($savedTable);
 						$('#able-vts').append(thisObj.getIconCredit());
-						thisObj.showVtsAlert( this.translate( 'vtsCancel', 'Cancelling saving. Any edits you made have been restored in the VTS table.' ) );
+						thisObj.showVtsAlert( thisObj.translate( 'vtsCancel', 'Cancelling saving. Any edits you made have been restored in the VTS table.' ) );
 					}
 				});
 			}
