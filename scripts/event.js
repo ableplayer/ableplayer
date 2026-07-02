e0a1e8f20f60b4eaf780e118c272a9bcef1aee6e
Joe Dolson <design@joedolson.com>
2026-07-02 18:26:29 -0500

    If event triggered from outside the player by a button with no classes, $.attr('class') would be undefined

--- scripts/event.js
+++ scripts/event.js
@@ -229,7 +229,8 @@ function addEventFunctions(AblePlayer) {
 		if ( this.$focusedElement && null !== this.activeMedia ) {
 			$mediaParent = $( '#' + this.activeMedia ).closest( '.able' );
 			if ( ( this.$focusedElement).is('button') ) {
-				classList = this.$focusedElement.attr("class").split(/\s+/);
+				classList = this.$focusedElement.attr("class");
+				classList = ( typeof classList === 'string' ) ? classList.split(/\s+/) : [];
 				$.each(classList, function(index, item) {
 					if (item.substring(0,20) === 'able-button-handler-') {
 						$mediaParent.find('div.able-controller div.' + item).trigger('focus');
