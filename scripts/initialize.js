27209edc9d363eb5e69951dcec65d698838c0593
Joe Dolson <design@joedolson.com>
2026-09-23 12:39:46 -0500

    Inject src attribute as an additional `source`
    Fixes #788

--- scripts/initialize.js
+++ scripts/initialize.js
@@ -375,11 +375,15 @@ import DOMPurify from 'dompurify';
 	 * @returns {Array} Filtered array of sources.
 	 */
 	AblePlayer.prototype.getSources = function () {
+		let videoSource = this.media.getAttribute('src');
+		let sourceSource = document.createElement( 'source' );
+		sourceSource.src = videoSource;
 		let sources = this.media.querySelectorAll('source');
 		let newSources = Array.from(sources).filter(source => {
 			const media = source.getAttribute('media');
 			return (window.matchMedia(media) && window.matchMedia(media).matches);
 		});
+		newSources.push(sourceSource);
 		if ( newSources.length === 0 && sources.length > 0 ) {
 			// If no sources match the media query, return the original sources and allow browser to handle.
 			if ( this.debug ) {
